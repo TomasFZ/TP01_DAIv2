9ee9bbe38e387d04726a65e708dc075416ab204a
TOMAS FERNANDEZ ZABALA <47394082@ort>
2024-04-12 10:22:05 -0300

    indeed riko kaboom

--- TP01_DAI/src/controllers/eventos-controller.js
+++ TP01_DAI/src/controllers/eventos-controller.js
@@ -11,6 +11,24 @@ console.log("holaaaa")
 
 
 controller.get("/", (req, res) => {
+    var queryFilters = Object.keys(req.query).filter((key) => key.includes("name") | key.includes("category") | key.includes("startDate") | key.includes("tag"))
+    //if (Object.keys(req.query).length > "0")
+    console.log(queryFilters[0])
+    var bool = false
+    for(var i=0; i < queryFilters.length; i++)
+    {
+        if(queryFilters[i] == "name" | queryFilters[i] == "category" | queryFilters[i] == "tag" | queryFilters[i] == "startDate"){
+            bool = true
+        }
+    }
+    if (bool)
+    {
+        console.log("ok ahora si")
+        return res.status(500).send("sgcx") //aca manda el evento buscado
+    }
+    else
+    {
+        console.log("escefmdiknoigd")  //aca manda la lista completa de eventos
     // try {
     //     const limit = req.query.limit;
     //     const offset = req.query.offset;
@@ -25,26 +43,30 @@ controller.get("/", (req, res) => {
         const offset = req.query.offset;
         const allEvents = eventService.getAllEvents(limit, offset);
         console.log("los all events: " + allEvents);
+        
         return res.send(allEvents);
+    }
 });
 
 //busqueda de un evento
-controller.get("/:nombre, categoria, fecha, tag", (req, res) => {
-    const eventoBuscado = eventService.getEventBuscado(limit, offset, nombre, categoria, fecha, tag);
+controller.get("/", (req, res) => {
+    console.log("tablos controla mi mente")
+    const eventoBuscado = eventService.getEventBuscado(limit, offset, req.query.nombre, req.query.categoria, req.query.fecha, req.query.tag);
     if (eventoBuscado != null)
     {
-        return res.status(201).send( //verificar que el nombre sea string, que la fecha sea posible, etc
-            {
-                // "id": body.id, 
-                // "nombre": body.nombre, 
-                // "fecha": body.fecha, 
-                // "categoria": body.categoria,
-                // "tags": body.tags
-
+        // return res.status(201).send( //verificar que el nombre sea string, que la fecha sea posible, etc
+        //     {
+        //         // "id": body.id, 
+        //         // "nombre": body.nombre, 
+        //         // "fecha": body.fecha, 
+        //         // "categoria": body.categoria,
+        //         // "tags": body.tags
+        //         eventoBuscado
                 
 
-            }
-            )
+        //     }
+        //     )
+        return res.send(eventoBuscado);
     }
     else
     {
--- TP01_DAI/src/servicios/servicios-Eventos.js
+++ TP01_DAI/src/servicios/servicios-Eventos.js
@@ -3,6 +3,7 @@
 
 //ATENCIÓN: PARA LA PRIMERA ENTREGA SE TIENE QUE ENVIAR LOS CONTROLLERS CON TODO LO DEMÁS HARDCODEADO. ES DECIR, LOS OBJETOS EVENTO DE ACA SON ESCRITOS EN VEZ DE LLAMAR A LA DB ETC ETC. 
 
+
 export default class EventService{
     getAllEvents(pageSize, reqPage){
         const query = "select * from events limit $(pageSize) offset $(reqPage) inner join categories on events.id_event_category = event_categories.id inner join "  //continuar haciendo los inner join de localizacion, usuario creador, etc.   //ver si en realidad tiene que ser la query en events-repository.js           //en realidad es pg. y el query pero por ahora a hardcodear
@@ -54,15 +55,14 @@ export default class EventService{
 
     getEventBuscado(pageSize, reqPage, nombre, categoria, fecha, tag)
     {//tal vez se puede hacer un vector que contenga estos parametros y vaya buscando uno por uno en un ciclo. (cuando este la db)
-        const query = "" //tiene que ser UN solo evento especifico en base a los filtros
-        const listaEventosBuscados = query.execute(); 
+        //const query = "" //tiene que ser UN solo evento especifico en base a los filtros. lo vamos a harcdodear asi que por ahora nada de bd
+        //const listaEventosBuscados = query.execute(); 
         // const eventoBuscado = listaEventosBuscados.filter(evento => {
         //     if(evento.nombre === nombre, evento.categoria === categoria, evento.fecha === fecha, evento.tag === tag){
         //         return evento
         //     }else return null;
         //     }
         // )
-
 
         const eventoBuscado = {
                 "id": 2, 
