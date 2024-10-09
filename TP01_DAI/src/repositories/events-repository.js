588cb5dc0b1a2d9d1dfeabaa3dd37db4c79b46bc
Temporaly <ezequiel.boulet@gmail.com>
2024-10-09 12:10:51 -0300

    TP de mierda

--- TP01_DAI/src/repositories/events-repository.js
+++ TP01_DAI/src/repositories/events-repository.js
@@ -432,20 +432,22 @@ async getAllLocations(userId, limit, offset)
 
 async getOneLocation(id, id_creator_user)
 {
-    if(id_creator_user === null){
+    if(id){ //OG line: if(id_creator_user === null)
     const sql = "Select * From event_locations Where id = $1"
     console.log("HOLLASK SIFJSKHFUSH AQUI SFL "+sql)
     const params = [id]
+    console.log("Hace console log de id " + id)
     const result = await this.DBClient.query(sql, params)
-    console.log("nombre de la location: "+result.name)
+    console.log(result.rows[id-1].name)
     return result.rows
     }else{
     const sql = "Select * From event_locations Where id = $1 and id_creator_user = $2"
         console.log("IDCREATORUSER NO ES NULL: " + id_creator_user)
     console.log("HOLLASK SIFJSKHFUSH AQUI SFL "+sql)
     const params = [id, id_creator_user]
+    console.log("Hace console log de id " + id)
     const result = await this.DBClient.query(sql, params)
-    console.log("nombre de la location: "+result.name)
+    console.log("nombre de la location: "+result)
     return result.rows
     }
 }
